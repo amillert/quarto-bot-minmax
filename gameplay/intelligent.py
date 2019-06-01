61a6f7bd665d2addffe596e86abef316c708c893
Albert Millert <amillert@roq.ad>
2019-06-02 00:18:56 +0200

    Made the bot work

--- gameplay/intelligent.py
+++ gameplay/intelligent.py
@@ -6,44 +6,41 @@ import utils.functions as f
 
 
 def bot_move(possible_positions, possible_pawns, pawn_picked_for_bot, board):
-    # current situation
+    print(f"QUARTO-BOT has been given {pawn_picked_for_bot} pawn.")
     root = Move(copy.deepcopy(board), copy.deepcopy(possible_positions), copy.deepcopy(possible_pawns))
-    # generating moves
     f.recursive_combinations(copy.deepcopy(possible_positions), copy.deepcopy(possible_pawns), board, root)
     # finding first good move
     steps = f.minmax(root, None)
-    print(steps)
     if steps:
-        print("from minmax")
-        bot_chosen_position = [x[0] for x in steps if x[1] == pawn_picked_for_bot][0]
+        bot_picked_position = [x[0] for x in steps if x[1] == pawn_picked_for_bot][0]
         steps = steps[:-1]
-        pawn_chosen_for_player = steps[0][1]
+        pawn_picked_for_player = steps[0][1]
     else:
-        print("from random")
-        bot_chosen_position = random.choice(possible_positions)
-        pawn_chosen_for_player = random.choice(possible_pawns)
-    print(bot_chosen_position)
-    possible_positions = [x for x in possible_positions if x != bot_chosen_position]
-    i, j = [int(x) for x in bot_chosen_position]
+        bot_picked_position = random.choice(possible_positions)
+        pawn_picked_for_player = random.choice(possible_pawns)
+    print(f"And chose the position {bot_picked_position} for it.")
+    possible_positions = [x for x in possible_positions if x != bot_picked_position]
+    i, j = [int(x) for x in bot_picked_position]
     board[i][j] = pawn_picked_for_bot
-    possible_pawns = [x for x in possible_pawns if x != pawn_chosen_for_player]
-    return possible_positions, possible_pawns, pawn_chosen_for_player, board
+    print("The board looks like so:")
+    f.print_board(board)
+    possible_pawns = [x for x in possible_pawns if x != pawn_picked_for_player]
+    return possible_positions, possible_pawns, pawn_picked_for_player, board
 
 
-def user_move(possible_positions, possible_pawns, pawn_chosen_for_player, board):
-    print(f"Pawn chosen for you is: {pawn_chosen_for_player}")
-    print("Possible positions are: ", possible_positions)
-    print("Where do You wish to place Your pawn? ")
-    user_picked_position = input()
-    assert len(str(user_picked_position)) == 2, "You must pass positions on the board"
-    possible_positions = [x for x in possible_positions if x != str(user_picked_position)]
-    # possible_pawns = [x for x in possible_pawns if x != pawn_chosen_for_player]
-    print("Possible pawns are: ", possible_pawns)
-    print("What pawn do You wish to choose for the bot? ")
+def user_move(possible_positions, possible_pawns, pawn_picked_for_player, board):
+    print(f"PLAYER has been given {pawn_picked_for_player} pawn.")
+    print(f"There are {possible_positions} possible positions to place it.")
+    print("Where do You wish to place the pawn? ")
+    player_picked_position = input()
+    assert len(str(player_picked_position)) == 2, "You must pass positions on the board"
+    possible_positions = [x for x in possible_positions if x != str(player_picked_position)]
+    i, j = [int(x) for x in player_picked_position]
+    board[i][j] = pawn_picked_for_player
+    print("The board looks like so:")
+    f.print_board(board)
+    print(f"There are {possible_pawns} possible pawns to choose from.")
+    print("Which one do You wish to pick for the bot? ")
     pawn_picked_for_bot = input()
     possible_pawns = [x for x in possible_pawns if x != pawn_picked_for_bot]
-    i, j = [int(x) for x in user_picked_position]
-    board[i][j] = pawn_picked_for_bot
-    f.print_board(board)
-    print()
     return possible_positions, possible_pawns, pawn_picked_for_bot, board
