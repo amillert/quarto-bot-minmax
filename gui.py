fc375f1d4d54ffd870035c0b93f1a16b0a0488dd
Michał Mirończuk <michalmironczuk1@gmail.com>
2019-06-26 22:16:34 +0200

    Improved GUI

--- gui.py
+++ gui.py
@@ -15,7 +15,7 @@ board = [[" ", " ", " ", " "], [" ", " ", " ", " "], [" ", " ", " ", " "], [" ",
 move = 0
 last_picked_pawn = None
 
-gameRoot = []
+block = 0
 labels = []
 figures = []
 fields = []
@@ -26,8 +26,15 @@ class Tile(tk.Label):
     def __init__(self, parent, photo, figure):
         super(Tile, self).__init__(parent)
         self.bind('<Button-1>', self.chooseFigure)
+        self.isEmpty = False
         self.config(image=photo)
         self.figure = figure
+
+    def setIsEmpty(self):
+        self.isEmpty = True
+    
+    def getIsEmpty(self):
+        return self.isEmpty
 
     def chooseFigure(self, event):
         global pos
@@ -35,28 +42,40 @@ class Tile(tk.Label):
         global pawn_picked_for_player
         global board
         global last_picked_pawn
-        pawn_picked_for_bot = self.figure
-        pawns = [x for x in pawns if x != pawn_picked_for_bot]
-        changeSelectedPawn(pawn_picked_for_bot)
-        self.setPhoto(16)
-        last_picked_pawn = pawn_picked_for_bot
-        print("QUARTO-BOT's move.")
-        global labels
-        labels[0]['text'] = "Ruch Quarto Bota !"
-        pos, pawns, pawn_picked_for_player, board, bot_picked_position = intelligent.bot_move(
-            pos, pawns, pawn_picked_for_bot, board) 
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print()
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Bot picked position")
-        print(bot_picked_position)
-        for idx, field in enumerate(fields):
-            if field.getPosition() == str(bot_picked_position):
-                field.setPhoto(int(last_picked_pawn, 2))
-        changeSelectedPawn('10000')
-        printConsoleBoard()
-        checkIsWinning("QUARTO BOT")
-        userMove()
+        global block
+        if block == 1:
+            if self.getIsEmpty() == True:
+                changeNotificationText("Wybierz figurkę.")
+                print("Choose pawn")
+            else:
+                pawn_picked_for_bot = self.figure
+                pawns = [x for x in pawns if x != pawn_picked_for_bot]
+                changeSelectedPawn(pawn_picked_for_bot)
+                self.setPhoto(16)
+                last_picked_pawn = pawn_picked_for_bot
+                print("QUARTO-BOT's move.")
+                changeNotificationText("Ruch Quarto Bot !")
+                print(len(labels))
+                pos, pawns, pawn_picked_for_player, board, bot_picked_position = intelligent.bot_move(
+                    pos, pawns, pawn_picked_for_bot, board) 
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print()
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print("Bot picked position")
+                print(bot_picked_position)
+                for idx, field in enumerate(fields):
+                    if field.getPosition() == str(bot_picked_position):
+                        field.setPhoto(int(last_picked_pawn, 2)) 
+                        field.setIsNotEmpty()
+                self.setIsEmpty()
+                changeSelectedPawn('10000')
+                printConsoleBoard()
+                checkIsWinning("QUARTO BOT")
+                userMove()
+                block = 0     
+        elif block == 0:
+            changeNotificationText("Wybierz pole.")
+            print("Choose field first")
 
     def getFigure(self):
         print(self.figure)
@@ -70,9 +89,16 @@ class Field(tk.Label):
         super(Field, self).__init__(parent)
         self.bind('<Button-1>', self.setField)
         self.config(image=photo)
+        self.isEmpty = True
         self.number = number
         self.X = i
         self.Y = j
+
+    def setIsNotEmpty(self):
+        self.isEmpty = False
+    
+    def getIsEmpty(self):
+        return self.isEmpty
 
     def setPhoto(self, pickedPawnInDecimal):
         print("in field {pickedPawnInDecimal}")
@@ -83,25 +109,31 @@ class Field(tk.Label):
         global pawns
         global pawn_picked_for_player
         global board
-        print("Selected field:")
-        play.player_picked_position = str(self.X) + str(self.Y)
-        print(play.player_picked_position)
-        print('Pawn to field')
-        print(pawn_picked_for_player)
-        self.setPhoto(int(str(pawn_picked_for_player), 2))
-        changeSelectedPawn('10000')
-        assert len(str(play.player_picked_position)) == 2, "You must pass positions on the board"
-        pos = [x for x in pos if x != str(play.player_picked_position)]
-        i, j = [int(x) for x in play.player_picked_position]
-        board[i][j] = pawn_picked_for_player
-        print("The board looks like so:")
-        printConsoleBoard()
-        checkIsWinning("USER")
-        print(f"There are {pawns} possible pawns to choose from.")
-        print("Which one do You wish to pick for the bot? ")
-        # newImage, figure = self.loadFigureImg(chosenFigure)
-        # self.config(image=photos[int(chosenFigure, 2)])
-        # chosenFigure = ""
+        global block
+        if block == 0:
+            if self.getIsEmpty() == False:
+                changeNotificationText("Pole zajete.")
+                print("Field is busy")
+            else:
+                changeNotificationText("Wybierz figurkę.")
+                play.player_picked_position = str(self.X) + str(self.Y)
+                self.setPhoto(int(str(pawn_picked_for_player), 2))
+                changeSelectedPawn('10000')
+                assert len(str(play.player_picked_position)) == 2, "You must pass positions on the board"
+                pos = [x for x in pos if x != str(play.player_picked_position)]
+                i, j = [int(x) for x in play.player_picked_position]
+                board[i][j] = pawn_picked_for_player
+                print("The board looks like so:")
+                printConsoleBoard()
+                checkIsWinning("USER")
+                print(f"There are {pawns} possible pawns to choose from.")
+                print("Which one do You wish to pick for the bot? ")
+                self.setIsNotEmpty()
+                block = 1
+        elif block == 1:
+            changeNotificationText("Wybierz figurkę.")
+            print("Choose pawn first")
+
 
     def loadFigureImg(self, figure):
         script_dir = os.path.dirname(__file__)
@@ -111,9 +143,10 @@ class Field(tk.Label):
         return photo1, figure
 
     def getPosition(self):
-        print('bo')
-        print(str(self.X) + str(self.Y))
         return str(self.X) + str(self.Y)
+
+    def getPhoto(self):
+        return self.cget('image')
 
 class Main():
 
@@ -122,13 +155,9 @@ class Main():
         self.player1 = tk.StringVar()
         self.player2 = tk.StringVar()
         self.createWidgets()
+        self.start()
 
     def createWidgets(self):
-        self.mainFrame = tk.Frame(self.parent)
-        tk.Label(self.mainFrame, text="Quarto", font=("", 50)).pack()
-        tk.Button(self.mainFrame, text='Start',
-                  command=self.start).pack()
-        self.mainFrame.pack(padx=10, pady=10)
         self.player1Frame = tk.Frame(self.parent)
         self.selectFrame = tk.Frame(self.parent, background='#ececec')
         self.gameFrame = tk.Frame(self.parent)
@@ -196,9 +225,8 @@ class Main():
         
 
     def start(self):
-        self.mainFrame.forget()
         self.createboard()
-        notifi = tk.Label(text="Twój ruch !", font=("Roboto", 13))
+        notifi = tk.Label(text="Powiadomienie", font=("Roboto", 13))
         notifi.grid(row=0, column=2)
         labels.append(notifi)
         self.player1Frame.grid(row=1, column=0)
@@ -221,11 +249,15 @@ class Main():
         label.grid(row=1, column=0)
         figures.append(tile)  
 
+def changeNotificationText(text):
+    labels[0]['text'] = text
+
 def userMove():
-    labels[0]['text'] = "Twój ruch !"
+    changeNotificationText("Twój Ruch !")
     changeSelectedPawn(pawn_picked_for_player)
     deletePawnFromChoose(pawn_picked_for_player)
     print(f"PLAYER has been given {pawn_picked_for_player} pawn.")
+    figures[int(pawn_picked_for_player, 2)].setIsEmpty()
     global pos
     print(f"There are {pos} possible positions to place it.")
     print("Where do You wish to place the pawn? ")
@@ -234,7 +266,6 @@ def deletePawnFromChoose(pickedPawn):
     figures[int(pickedPawn, 2)].setPhoto(16)
     
 def changeSelectedPawn(pickedPawn):
-    print(int(pickedPawn, 2))
     figures[16].setPhoto(int(pickedPawn, 2))
 
 def printConsoleBoard():
@@ -250,15 +281,15 @@ def checkIsWinning(who):
         print("The game has been won by the " + who)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         if who == "QUARTO BOT":
-            labels[0]['text'] = "PRZEGRAŁEŚ !"
+            changeNotificationText("PRZEGRAŁEŚ !")
             popup_bonus(who)
         elif who == "USER":
-            labels[0]['text'] = "WYGRAŁEŚ !"
+            changeNotificationText("WYGRAŁEŚ !")
             popup_bonus(who)
 
 def popup_bonus(who):
     win = tk.Toplevel()
-    win.wm_title("Koniec gr")
+    win.wm_title("Koniec gry")
 
     if who == "QUARTO BOT":
         l = tk.Label(win, text="PRZEGRAŁEŚ !")
@@ -278,5 +309,5 @@ def end(event):
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Quarto")
-    gameRoot.append(Main(root))
+    Main(root)
     root.mainloop()
